5f7712bc9b46064dcf0737f33db37fc039798a3d
kharozim <kharozim.wrk@gmail.com>
2026-05-04 11:37:24 +0700

    Update lib_call version to 0.5.0 and fix artifact name in jitpack.yml

--- lib_call/build.gradle.kts
+++ lib_call/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
         from(components["release"])
         groupId = "com.neo"
         artifactId = "lib_call"
-        version = "0.4.0"
+        version = "0.5.0"
       }
     }
   }
