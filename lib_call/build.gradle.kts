9c7f5296d1ea50287b6fe396f7dcd5af0644bb7e
kharozim <kharozim.wrk@gmail.com>
2026-09-22 14:46:55 +0700

    Update version lib 0.9.10

--- lib_call/build.gradle.kts
+++ lib_call/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
         from(components["release"])
         groupId = "com.neo"
         artifactId = "lib_call"
-        version = "0.9.9"
+        version = "0.9.10"
       }
     }
   }
